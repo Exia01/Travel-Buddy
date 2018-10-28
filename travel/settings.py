99cf26387219bc7ee8d9901c00549b5f23286753
Jose <sixgg4@gmail.com>
2018-10-28 14:42:48 -0400

    Need to finish setting up

--- travel/settings.py
+++ travel/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '8ylq)@-m(5kt3^1o9bcn=b$or5gq0%l&z!%69$ky(zx187+6h7'
+SECRET_KEY = '8ylq)@-m(5kt3^1o9bcn=b$or5gq0%l&z!%69$ky(zx187+6h7'
 # Converted to system variable that can be set secretly
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+# SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
